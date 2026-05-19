4952d84838cd7666bfe38727ac8e1e61fe0746d2
Eliakim Paulino França <eliakim.paulino@gmail.com>
2026-05-19 20:29:25 -0300

    feat: Add core utilities and helpers for app functionality
    - Updated pubspec.yaml to include new dependencies: url_launcher, intl, firebase_storage, connectivity_plus, http, get_storage, and logger.
    - Created app.dart to define the main app structure with light and dark themes.
    - Added text_strings.dart for centralized text constants used throughout the app.
    - Implemented device_utility.dart for device-related functionalities such as keyboard management and screen dimensions.
    - Developed formatter.dart for formatting dates, currencies, and phone numbers.
    - Introduced cloud_helper_functions.dart for Firebase storage operations and snapshot state management.
    - Created helper_functions.dart for common UI and utility functions.
    - Implemented network_manager.dart to manage network connectivity status and handle changes.
    - Added pricing_calculator.dart for calculating product prices, taxes, and shipping costs.
    - Developed http_client.dart for simplified HTTP requests.
    - Created storage_utility.dart for local storage management using GetStorage.
    - Implemented logger.dart for structured logging throughout the app.
    - Added validation.dart for input validation functions for email, password, and phone numbers.

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class TColors {
+  TColors._();
+
   // App theme colors
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
@@ -18,7 +20,7 @@ class TColors {
 
   // Background Container colors
   static const Color lightContainer = Color(0xFFF6F6F6);
-  static Color darkContainer = TColors.white.withOpacity(0.1);
+  static Color darkContainer = TColors.white.withValues(alpha: 0.5);
 
   // Button colors
   static const Color buttonPrimary = Color(0xFF4b68ff);
