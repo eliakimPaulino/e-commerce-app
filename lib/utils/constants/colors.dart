a4b11ad5e2f743e21ac551a228510a0a4997f107
Eliakim Paulino França <eliakim.paulino@gmail.com>
2026-05-24 11:56:04 -0300

    feat: update color constants and apply them to theme backgrounds

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -14,9 +14,9 @@ class TColors {
   static const Color textWhite = Colors.white;
 
   // Background colors
-  static const Color light = Color(0xFFF6F6F6);
-  static const Color dark = Color(0xFF272727);
-  static const Color primaryBackground = Color(0xFFF3F5FF);
+  static const Color light = Color(0xFFF6F7FA);
+  static const Color dark = Color(0xFF434A54);
+  static const Color primaryBackground = Color(0xFFE6E9ED);
 
   // Background Container colors
   static const Color lightContainer = Color(0xFFF6F6F6);
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/theme/custom_themes/elevated_button_theme.dart';
 import 'package:e_commerce/utils/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,7 @@ class TAppTheme {
     fontFamily: 'Poppins',
     brightness: Brightness.light,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
+    scaffoldBackgroundColor: TColors.light,
     textTheme: TTextTheme.lightTextTheme,
     elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
   );
@@ -19,7 +20,7 @@ class TAppTheme {
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.black,
+    scaffoldBackgroundColor: TColors.dark,
     textTheme: TTextTheme.darkTextTheme,
     elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
   );
